eda8c5a36e82b2468e21367ae4d16b635276ad7b
Thomas Kluge <thklchen@gmail.com>
2025-10-17 11:41:18 +0200

    work on ui

--- src/app/components/logview/logview.ts
+++ src/app/components/logview/logview.ts
@@ -4,24 +4,35 @@ import { DatePipe } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 import { OptionPanelComponent } from '../controls/optionpanel/optionpanel';
 import { SelectOption } from '../../models/types';
+import { BridgeService } from '../../services/bridge.service';
 
 @Component({
   selector: 'LogView',
-  imports: [DatePipe,TranslateModule,OptionPanelComponent],
+  imports: [DatePipe, TranslateModule, OptionPanelComponent],
   templateUrl: './logview.html',
   styleUrl: './logview.scss'
 })
 export class LogView {
   protected readonly eventStore = inject(BridgeEventStore);
+  protected readonly bridgeService = inject(BridgeService);
+
+  log_level = computed(() => {
+    const bi = this.bridgeService.getBridgeInfo();
+    if (bi() !== null) {
+      return bi()!.log_level;
+    };
+    return "";
+  })
+
   logViewOpen = model<boolean>(false);
   toggleLogView = output();
 
   logOptions = signal<SelectOption[]>([
-    {label:"Info",isSelected:false},
-    {label:"Debug",isSelected:false},
-    {label:"Warning",isSelected:false},
-    {label:"Error",isSelected:false}
- ])
+    { label: "Info", isSelected: false },
+    { label: "Debug", isSelected: false },
+    { label: "Warning", isSelected: false },
+    { label: "Error", isSelected: false }
+  ])
 
   protected readonly reversedEntries = computed(() => {
     return [...this.eventStore.entities()].reverse();
@@ -35,7 +46,9 @@ export class LogView {
     this.eventStore.clear();
   }
 
-  logOptionChange(event:any) {
-    
+
+
+  logOptionChange(event: any) {
+    console.log(event);
   }
 }
