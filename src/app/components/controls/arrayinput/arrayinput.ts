8a4bba590644aa2e6b2aba34b476af6e8441dc59
Thomas Kluge <thklchen@gmail.com>
2025-11-08 21:54:22 +0100

    make this work

--- src/app/components/controls/arrayinput/arrayinput.ts
+++ src/app/components/controls/arrayinput/arrayinput.ts
@@ -1,10 +1,10 @@
 import { ExpansionPanelComponent } from '@/app/components/controls/expansionpanel/expansionpanel';
-import { Component, computed, input, model, signal } from '@angular/core';
+import { Component, computed, effect, input, model, output, signal } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'ArrayInputComponent',
-  imports: [ExpansionPanelComponent,TranslateModule],
+  imports: [ExpansionPanelComponent, TranslateModule],
   templateUrl: './arrayinput.html',
   styleUrl: './arrayinput.scss',
 })
@@ -12,22 +12,35 @@ export class ArrayInputComponent {
   items = model<string[]>([]);
   panelIsOpen = model<boolean | null>(null);
   inputText = signal<string>('');
-
+  changed = output<string[]>();
   title = computed(() => {
-    return this.items() ? this.items().join("|") : ""
+    return Array.isArray(this.items()) ? this.items().join("|") : ""
   })
 
 
+  constructor() {
+     
+  }
+
   applyInput(event: any): void {
     if (event !== null) {
-      this.inputText.set(event.target.value)
-      const old = this.items() ?? [];
-      old.push(event.target.value)
-      this.items.set([...old]);
-      setTimeout(()=>{
-        this.inputText.set("");
-      },1)
+      const text:string = event.target.value
+      const newItems:string[] = Array.isArray(this.items()) ? this.items() : [];
+      newItems.push(text);
+      this.items.set([...newItems]);
+      this.inputText.set("");
+      this.changed.emit(newItems);
     }
   }
+
+
+  remove(item: string) {
+    if (this.items()) {
+      const newitems = this.items().filter(i => i !== item);
+      this.items.set([...newitems]);
+      this.changed.emit(newitems);
+    }
+  }
+
 
 }
